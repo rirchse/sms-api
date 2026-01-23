03da1452f7d785b38c65e18534f76fbddee2246c
rirchse <rirchse@gmail.com>
2026-01-23 11:37:13 +0600

    School model creation with table schema...

---
+++ app/Http/Controllers/Api/AdmissionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdmissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+      $this->validation($request, [
+        //
+      ]);
+
+      $data = $request->all();
+      if(isset($data['_method']))
+      {
+        unset($data['_method']);
+      }
+
+      try {
+        Student::create($data);
+      }
+      catch(\Exception $e)
+      {
+        return $e->getMessage();
+      }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::controller(AdmissionController::class)->group(function ()
+{
+  Route::post('/admission', 'admission');
+});
 
 Route::middleware('auth:sanctum')->group(function ()
 {
